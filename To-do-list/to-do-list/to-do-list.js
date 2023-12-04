85d26967b1c8f2e13f2fe9ad635ca34d95f5bf8f
HarryNwa <harrynwa90@gmail.com>
2023-12-04 16:27:53 +0100

    update

---
+++ To-do-list/to-do-list/to-do-list.js
@@ -0,0 +1,49 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function createTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const li = document.createElement("li");
+        const createdAt = new Date().toLocaleString();
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <span class="createdAt">${createdAt}</span>
+            <button onclick="deleteTask(this)">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
+
+function deleteTask(button) {
+    const li = button.parentNode;
+    taskList.removeChild(li);
+}
+
+//  code to fetch all tasks from Spring Boot backend
+fetch('http://localhost:8080/api/task')
+    .then(response => response.json())
+    .then(tasks => {
+        console.log(tasks);
+    })
+    .catch(error => console.error('Error fetching tasks:', error));
+
+const newTask = {
+    text: 'Do something',
+    completed: false
+};
+
+fetch('http://localhost:8080/api/task', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newTask)
+})
+    .then(response => response.json())
+    .then(addedTask => {
+        console.log(addedTask);
+    })
+    .catch(error => console.error('Error adding task:', error));
